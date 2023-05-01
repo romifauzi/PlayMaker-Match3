24faee98aa7a05aca1734065a03ebf310803a41b
Romi Fauzi <romi.fauzi@gmail.com>
2023-05-01 16:07:04 +0700

    somewhat working basic match3 examples using PlayMaker

---
+++ Assets/Resources/Scripts/MaterialName.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MaterialName : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log (GetComponent<Renderer>().material.name);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
